df0b14bcb4e7bad5754fd36fcaf8c0e93502864e
arjyo851 <arjyo77@gmail.com>
2023-04-28 02:11:39 +0530

    added build command

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json({ extended: false }));
 app.use('/api/invoices', require('./routes/invoices'));
 
 // Serve static assets in production
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
@@ -22,7 +22,7 @@ app.use('/api/invoices', require('./routes/invoices'));
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
-// }
+}
 
 // Start server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
